72372765df23906541dfbd538c98d0fc943b32ce
Ignace Mella <ignace.mella@telenet.be>
2024-10-13 23:48:31 +0200

    fix

--- domain.php
+++ domain.php
@@ -5,13 +5,11 @@ $domain = null;
 
 if (isset($_SERVER['HTTP_HOST'])) {
     $domain = $_SERVER['HTTP_HOST'];
-    echo "Using HTTP_HOST: $domain\n";
 } elseif (isset($argv[2])) {
     $domain = $argv[2];
     //set http_host to domain
     $_SERVER['HTTP_HOST'] = $domain;
     $cli = true;
-    echo "Using CLI argument: $domain\n";
 } else {
     if ($domain === null) {
         //log all args
