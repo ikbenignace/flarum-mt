5c2a9498c2af296e69fd17b4f846c117aba4d36c
Ignace Mella <ignace.mella@telenet.be>
2024-10-13 21:57:40 +0200

    test

--- domain.php
+++ domain.php
@@ -8,10 +8,13 @@ if (isset($_SERVER['HTTP_HOST'])) {
     echo "Using HTTP_HOST: $domain\n";
 } elseif (isset($argv[2])) {
     $domain = $argv[2];
+    //set http_host to domain
+    $_SERVER['HTTP_HOST'] = $domain;
     $cli = true;
     echo "Using CLI argument: $domain\n";
 } else {
     if ($domain === null) {
-        die('No domain specified.');
+        //log all args
+        die('No domain specified: ' . print_r($argv, true));
     }
 }
