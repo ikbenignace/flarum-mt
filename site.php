b493fc6ba0d3a07eeefa34a6d9fbd02cfe6d5238
Ignace Mella <ignace.mella@telenet.be>
2024-10-13 16:59:06 +0200

    fix

--- site.php
+++ site.php
@@ -45,6 +45,6 @@ require __DIR__.'/vendor/autoload.php';
 
 return Flarum\Foundation\Site::fromPaths([
     'base' => __DIR__,
-    'public' => __DIR__.'//domains/'. $_SERVER['SERVER_NAME'] .'/public',
-    'storage' => __DIR__.'/domains/'. $_SERVER['SERVER_NAME'] .'/storage',
+    'public' => __DIR__.'/domains/'.$_SERVER['SERVER_NAME'].'/public',
+    'storage' => __DIR__.'/domains/'.$_SERVER['SERVER_NAME'].'/storage',
 ]);
