77219e7bd5207bfe2acb13de46532b04d7703d9b
Ignace Mella <ignace.mella@telenet.be>
2024-10-17 12:09:12 +0200

    fix

--- rootfs/etc/cont-init.d/03-config.sh
+++ rootfs/etc/cont-init.d/03-config.sh
@@ -251,7 +251,7 @@ adminUser:
 settings:
   forum_title: ${domain}
 EOL
-      yasu flarum:flarum php flarum install --file=/tmp/config.yml
+      yasu flarum:flarum cd /opt/flarum && php flarum install --file=/tmp/config.yml
       yasu flarum:flarum touch /data/assets/rev-manifest.json
       echo ">>"
       echo ">> WARNING: Flarum has been installed with the default credentials (flarum/flarum)"
